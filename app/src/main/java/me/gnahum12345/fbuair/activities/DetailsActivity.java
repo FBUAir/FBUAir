d5833dc520c69eedc3ede70bba4ad6cd3b625ffb
mariam234 <mariamdiallo@fb.com>
2018-07-18 10:15:12 -0700

    added details activity

---
+++ app/src/main/java/me/gnahum12345/fbuair/activities/DetailsActivity.java
@@ -0,0 +1,15 @@
+package me.gnahum12345.fbuair.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.gnahum12345.fbuair.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details2);
+    }
+}
