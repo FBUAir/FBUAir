8f1e5d5ebe0f8da541e5148924db7ba65e6c3afe
mariam234 <mariamdiallo@fb.com>
2018-07-17 19:29:00 -0700

    can undo contact change/adding, notifies of phone number/email duplicate, merges if possible

--- app/src/main/java/me/gnahum12345/fbuair/activities/AddContactActivity.java
+++ app/src/main/java/me/gnahum12345/fbuair/activities/AddContactActivity.java
@@ -1,9 +1,7 @@
 package me.gnahum12345.fbuair.Activities;
 
-import android.app.Activity;
 import android.content.ContentProviderOperation;
 import android.content.ContentProviderResult;
-import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.OperationApplicationException;
@@ -13,9 +11,8 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -25,7 +22,6 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -88,18 +84,63 @@ public class AddContactActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    JSONObject jsonUser5;
+    {
+        try {
+            // populate fields
+            jsonUser5 = new JSONObject()
+                    .put("name", "James Smith")
+                    .put("phone", "2039481726")
+                    .put("email", "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    JSONObject jsonUser6;
+    {
+        try {
+            // populate fields
+            jsonUser6 = new JSONObject()
+                    .put("name", "James")
+                    .put("phone", "2039481726")
+                    .put("email", "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    JSONObject jsonUser7;
+    {
+        try {
+            // populate fields
+            jsonUser7 = new JSONObject()
+                    .put("name", "Lo")
+                    .put("phone", "2938401927")
+                    .put("email", "");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     // views
     Button btAddContact;
+    RelativeLayout rlContactOptions;
     TextView tvUndo;
     TextView tvView;
-    RelativeLayout rlContactOptions;
+    RelativeLayout rlConflict;
+    TextView tvViewConflict;
+    TextView tvIgnoreConflict;
+    TextView tvConflictMessage;
 
-    // User contact ID
-    long contactId;
+    // user contact IDs
+    String contactId;
     String[] rawContactIds;
+    String lookupKey;
 
-    Uri contactUri;
+    // codes for options after adding contact
+    final static int SUCCESS = 1;
+    final static int PHONE_CONFLICT = 2;
+    final static int EMAIL_CONFLICT = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -107,9 +148,13 @@ public class AddContactActivity extends AppCompatActivity {
 
         // get references to views
         btAddContact = findViewById(R.id.btAddContact);
+        rlContactOptions = findViewById(R.id.rlContactOptions);
         tvUndo = findViewById(R.id.tvUndo);
         tvView = findViewById(R.id.tvView);
-        rlContactOptions = findViewById(R.id.rlContactOptions);
+        rlConflict = findViewById(R.id.rlConflict);
+        tvViewConflict = findViewById(R.id.tvViewConflict);
+        tvIgnoreConflict = findViewById(R.id.tvIgnoreConflict);
+        tvConflictMessage = findViewById(R.id.tvConflictMessage);
 
         // CLICK HANDLERS
         // add new contact when button is clicked
@@ -117,7 +162,9 @@ public class AddContactActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 try {
-                    addContact(jsonUser4);
+                    if (noConflict(jsonUser3)) {
+                        addContact(jsonUser3);
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -128,7 +175,7 @@ public class AddContactActivity extends AppCompatActivity {
         tvUndo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                undoContact();
+                undo();
             }
         });
 
@@ -136,7 +183,7 @@ public class AddContactActivity extends AppCompatActivity {
         tvView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                viewContact();
+                viewContact(contactId);
             }
         });
     }
@@ -147,6 +194,7 @@ public class AddContactActivity extends AppCompatActivity {
         String phone = user.getString("phone");
         String email = user.getString("email");
 
+        // start adding contact
         ArrayList<ContentProviderOperation> ops = new ArrayList<>();
         int rawContactInsertIndex = ops.size();
         // set user contact fields
@@ -181,78 +229,196 @@ public class AddContactActivity extends AppCompatActivity {
                 .build());
         try {
             // display options to undo and view
-            showOptions(true);
+            showOptions(SUCCESS, true);
             // create the new contact
             ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
-            // get contact ID (for viewing)
+            // get created contact ID (for viewing)
             final String[] projection = new String[] { ContactsContract.RawContacts.CONTACT_ID };
             final Cursor cursor = getContentResolver().query(results[0].uri, projection, null, null, null);
-            cursor.moveToNext();
-            contactId = cursor.getLong(0);
-            cursor.close();
-            // get raw contact IDs for each operation (for undoing)
-            rawContactIds = new String[results.length];
-            for (int i = 0; i < results.length; i++) {
-                rawContactIds[i] = String.valueOf(ContentUris.parseId(results[i].uri));
+            if (cursor != null && cursor.moveToNext()) {
+                contactId = String.valueOf(cursor.getLong(0));
+                cursor.close();
             }
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (OperationApplicationException e) {
+            // get created raw contact ID (for undoing)
+            rawContactIds = new String[] {String.valueOf(ContentUris.parseId(results[0].uri))};
+            // check if merge occured and notify user
+            if (merged (contactId)) {
+                Toast.makeText(this, "Contact was merged with duplicate", Toast.LENGTH_LONG).show();
+            }
+        } catch (RemoteException | OperationApplicationException e) {
             e.printStackTrace();
         }
     }
 
-    void undoContact() {
-        // hide undo and view options
-        showOptions(false);
-        // delete raw contact
-        ArrayList ops = new ArrayList();
-        ops.add(ContentProviderOperation.newDelete(ContactsContract.RawContacts.CONTENT_URI)
-                .withSelection(ContactsContract.RawContacts._ID + "=?", rawContactIds)
-                .build());
+    // checks for email and phone conflicts and shows conflict options if true
+    boolean noConflict(final JSONObject user) throws JSONException {
+        // get user info
+        final String phone = user.getString("phone");
+        final String email = user.getString("email");
+
+        // find phone number conflicts
+        final String phoneConflictId = getPhoneConflictId(phone);
+        if (phoneConflictId != null) {
+            // set conflict options click listeners (view conflict; add anyway) and show them
+            showOptions(PHONE_CONFLICT, true);
+            // set click listeners
+            tvViewConflict.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    viewContact(phoneConflictId);
+                }
+            });
+            tvIgnoreConflict.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        // check for more conflicts
+                        showOptions(PHONE_CONFLICT, false);
+                        addContact(user);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            return false;
+        }
+
+        // find email conflicts
+        final String emailConflictId = getEmailConflictId(email);
+        if (emailConflictId != null) {
+            // set conflict options click listeners (view conflict; add anyway) and show them
+            showOptions(EMAIL_CONFLICT, true);
+            tvViewConflict.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    viewContact(emailConflictId);
+                }
+            });
+            tvIgnoreConflict.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        showOptions(EMAIL_CONFLICT, false);
+                        addContact(user);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            return false;
+        }
+
+        // return false if no conflicts
+        return true;
     }
 
-    void viewContact() {
-        // create intent to view newly added contact
-        Uri contactUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, String.valueOf(contactId));
+    // undoes last raw contact operations
+    void undo() {
+        // hide undo and view options
+        showOptions(SUCCESS, false);
+        // delete raw contact
+        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
+        ops.add(ContentProviderOperation.newDelete(ContactsContract.RawContacts.CONTENT_URI)
+                .withSelection(ContactsContract.RawContacts._ID + " = ?", rawContactIds)
+                .build());
+        try {
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (RemoteException | OperationApplicationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // creates intent to view newly added contact
+    void viewContact(String contactId) {
+        Uri contactUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, contactId);
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(contactUri);
         startActivity(intent);
     }
 
-/*        // create intent to create new contact
-        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
-        // Sets the MIME type to match the Contacts Provider
-        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-        // insert user's info into intent
-        intent.putExtra(ContactsContract.Intents.Insert.NAME, name)
-                .putExtra(ContactsContract.Intents.Insert.EMAIL, email)
-                .putExtra(ContactsContract.Intents.Insert.PHONE, phone)
-                // insert email and phone types
-                .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
-                .putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_HOME)
-                // go back to app after saving the contact
-                .putExtra("finishActivityOnSaveCompleted", true);
-
-        // send the intent
-        startActivityForResult(intent, ADD_CONTACT_REQUEST);
+    // returns ID of contact matching phone number. if doesn't exist, returns null
+    public String getPhoneConflictId(String phone) {
+        // get URI path for given phone number
+        Uri lookupUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone));
+        // set columns to retrieve
+        String[] phoneNumberProjection = { ContactsContract.PhoneLookup._ID };
+        // lookup phone number
+        Cursor cursor = getContentResolver().query(lookupUri, phoneNumberProjection, null, null, null);
+        // if there is a result, return ID of matching contact
+        if (cursor != null && cursor.moveToFirst()) {
+            String id = String.valueOf(cursor.getLong(0));
+            cursor.close();
+            return id;
+        }
+        // return null if no matching contacts
+        return null;
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == ADD_CONTACT_REQUEST && resultCode == Activity.RESULT_OK) {
+    // returns ID of contact matching email address. returns null if no conflict/match
+    public String getEmailConflictId(String email) {
+        // get URI path for given email
+        Uri lookupUri = Uri.withAppendedPath(CommonDataKinds.Email.CONTENT_LOOKUP_URI, Uri.encode(email));
+        // set columns to retrieve
+        String[] emailProjection = { CommonDataKinds.Email.CONTACT_ID };
+        // lookup email
+        Cursor cursor = getContentResolver().query(lookupUri, emailProjection, null, null, null);
+        // if there is a result, return ID of matching contact
+        if (cursor != null && cursor.moveToFirst()) {
+            String id = String.valueOf(cursor.getLong(0));
+            cursor.close();
+            return id;
         }
-    }*/
+        // return null if no matching contacts
+        return null;
+    }
 
-    // show options after adding contact; hide after undoing contact
-    public void showOptions(boolean flag) {
-        if (flag) {
-            rlContactOptions.setVisibility(View.VISIBLE);
-            btAddContact.setVisibility(View.GONE);
+    // checks if there are multiple raw contacts for one contact ID (for notifying user of merge)
+    public boolean merged(String contactId) {
+        // get URI path for given ID
+        Uri lookupUri = ContactsContract.RawContacts.CONTENT_URI;
+        // no columns needed
+        String[] projection = { ContactsContract.RawContacts.CONTACT_ID , ContactsContract.RawContacts._ID};
+        // find where contact ID = user's contact id
+        String selection = ContactsContract.RawContacts.CONTACT_ID + "=" + contactId;
+        // lookup contact ID
+        Cursor cursor = getContentResolver().query(lookupUri, projection, selection, null, null);
+        // if there are multiple results, ID existed before adding contact and merge occured
+        if (cursor != null && cursor.getCount() > 1) {
+            cursor.close();
+            return true;
         }
+        return false;
+    }
+
+    // show appropriate options after adding contact based on whether it was successful or there was a conflict
+    public void showOptions(int optionsType, boolean show) {
+        // show options for no conflict
+        if (optionsType == SUCCESS) {
+            if (show) {
+                rlContactOptions.setVisibility(View.VISIBLE);
+                btAddContact.setVisibility(View.GONE);
+            }
+            else {
+                rlContactOptions.setVisibility(View.GONE);
+                btAddContact.setVisibility(View.VISIBLE);
+            }
+        }
+        // show options for conflict
         else {
-            rlContactOptions.setVisibility(View.GONE);
-            btAddContact.setVisibility(View.VISIBLE);
+            if (show) {
+                if (optionsType == PHONE_CONFLICT) {
+                    tvConflictMessage.setText(getResources().getString(R.string.phone_conflict_message));
+                }
+                else if (optionsType == EMAIL_CONFLICT) {
+                    tvConflictMessage.setText(getResources().getString(R.string.email_conflict_message));
+                }
+                rlConflict.setVisibility(View.VISIBLE);
+                btAddContact.setVisibility(View.GONE);
+            }
+            else {
+                rlConflict.setVisibility(View.GONE);
+                btAddContact.setVisibility(View.VISIBLE);
+            }
         }
     }
 }
