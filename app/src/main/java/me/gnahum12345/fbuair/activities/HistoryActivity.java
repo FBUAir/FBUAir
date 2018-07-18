c957ab25fbfb9cf2d2a0d372839192f04fa4e743
Gabriella Garcia <garciag2@fb.com>
2018-07-18 10:04:32 -0700

    details and history added

---
+++ app/src/main/java/me/gnahum12345/fbuair/activities/HistoryActivity.java
@@ -0,0 +1,7 @@
+package me.gnahum12345.fbuair.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class HistoryActivity extends AppCompatActivity{
+
+}
---
+++ app/src/main/java/me/gnahum12345/fbuair/adapters/HistoryAdapter.java
@@ -0,0 +1,24 @@
+package me.gnahum12345.fbuair.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class HistoryAdapter extends RecyclerView.Adapter<DiscoverAdapter.ViewHolder>{
+
+    @NonNull
+    @Override
+    public DiscoverAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiscoverAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
