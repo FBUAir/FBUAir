243ddb3f1bc74b6f376cd60990753c93d036f597
mariam234 <mariamdiallo@fb.com>
2018-07-20 18:46:46 -0700

    added welcome screen and started moving sign up to fragments

---
+++ app/src/main/java/me/gnahum12345/fbuair/activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package me.gnahum12345.fbuair.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.gnahum12345.fbuair.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
